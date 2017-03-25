edae7aed03c43a42ff32fa85a50d8802c39e615d
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-25 18:16:28 +0100

    guard testing suite up and running

--- micro-reddit/app/models/user.rb
+++ micro-reddit/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+	# validates :name, 	presence: true
+										# length: { minimum:4, maximux:12}
 end
--- micro-reddit/test/models/user_test.rb
+++ micro-reddit/test/models/user_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	def setup
+		@user = User.new(name: "Example User")
+	end
+
+	test "basic user is valid" do
+		assert @user.valid?
+	end
   # test "the truth" do
   #   assert true
   # end
