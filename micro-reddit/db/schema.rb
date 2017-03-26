a2300c19ba351fe62083878f35753a094838df74
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-26 16:50:11 +0200

    migrate to be friends with the newly merged User model

--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325163249) do
+ActiveRecord::Schema.define(version: 20170326143059) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user"
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
