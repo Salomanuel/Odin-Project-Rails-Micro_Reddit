cba58a342afb74300ce735dd71a1440c667b1e41
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-27 18:11:05 +0200

    add many validations for comments

--- micro-reddit/app/models/comment.rb
+++ micro-reddit/app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+	validates :user_id, presence: true
+	validates :post_id, presence: true
+	validates :body,		presence: true,
+											length: {maximum:140}
 end
--- micro-reddit/test/models/comment_test.rb
+++ micro-reddit/test/models/comment_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@comment = Comment.new(	body:"hai dettop una cazzata",
+														user_id:1,
+														post_id:1)
+	end
+
+	test "it should work, dammit" do
+		assert @comment.valid?
+	end
+
+	test "it should have an owner" do
+		@comment.user_id = nil
+		assert_not @comment.valid?
+	end
+
+	test "it should have a post" do
+		@comment.post_id = nil
+		assert_not @comment.valid?
+	end
+
+	test "it should have a body" do
+		@comment.body = nil
+		assert_not @comment.valid?
+	end
+
+	test "comment shouldn't be too long" do
+		@comment.body = "culo"*50
+		assert_not @comment.valid?
+	end
 end
