46220b7f329e719b6ee68c25f7d1d27cdcfef908
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-27 17:48:49 +0200

    generate and migrate post model

--- micro-reddit/app/models/user.rb
+++ micro-reddit/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-
 	validates :name, 			presence: true,
 												length: {maximum: 20}
 	validates :password, 	presence: true,
 												length: {maximum: 12}
+	has_many :posts
 end
